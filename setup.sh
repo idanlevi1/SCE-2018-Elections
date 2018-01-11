776aec7925d920b02f7c64a1f7cb83c30f0a22a4
Idan Levi <idanlevi2@gmail.com>
2018-01-12 00:51:37 +0200

    change url

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ sudo yum -y groupinstall "Development Tools"
 sudo easy_install pip
 
 #clone proejct repo
-git clone https://github.com/Watted/SCE-2018-Elections.git
+git clone https://github.com/idan/SCE-2018-Elections.git
 cd SCE-2018-Elections
 
 #install our app requirements
